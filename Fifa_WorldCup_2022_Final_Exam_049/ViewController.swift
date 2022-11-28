c29aca2c0aa4c443272157a1703d5261d8ef4eb5
parinyapt <parinyapt99@gmail.com>
2022-11-28 10:45:58 +0700

    Initial Commit

---
+++ Fifa_WorldCup_2022_Final_Exam_049/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fifa_WorldCup_2022_Final_Exam_049
+//
+//  Created by Parinya Termkasipanich on 28/11/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
